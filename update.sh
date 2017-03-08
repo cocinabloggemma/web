bb9f86fd9a33c601a0d364f690543cb5eee8248f
cocinabloggemma <cocinabloggemma@gmail.com>
2017-03-08 22:54:05 +0100

    mejoras en el batch 3

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MESSAGE="$@"
+REPO="github.com/cocinabloggemma/web.git"
 
 if [ "$MESSAGE" == "" ]; then
 	echo "Debes pasar como parametros los comentarios de la subida"
@@ -8,11 +9,19 @@ if [ "$MESSAGE" == "" ]; then
 fi
 
 
+
+
+REPO_URL="https://$REPO"
 if [ -f auth.cfg ]; then
 	. auth.cfg
-	REPO="https://$USER:$PASS@github.com/cocinabloggemma/web.git"
-else
-	REPO="https://github.com/cocinabloggemma/web.git"
+	if [ "$USER" !="" ]; then
+		if [ "$PASS" != "" ]; then
+			REPO_URL="https://$USER:$PASS@$REPO"
+		else
+			REPO_URL="https://$USER@$REPO"
+		fi
+
+	fi
 fi
 
 
